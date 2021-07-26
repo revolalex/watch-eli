95f394f6c9d86226afe3c242a108dc95f5baa46a
Alex <revolalex@gmail.com>
2021-07-26 22:32:35 +0200

    parrallax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,16 @@ import { withTranslation } from 'react-i18next';
 import CardLine from '../components/CardLine'
 import CarouselHero from '../components/CarouselHero'
 import "./Home.css"
-import { Parallax } from 'react-parallax';
+
 
 
 class Home extends Component {
-  Container = () => (
-    <Parallax blur={10} bgImage="../img/rolex-para.jpg" bgImageAlt="the cat" strength={200}>
-    </Parallax>
-);
   render() {
     return (
       <div>
-        <CarouselHero/>
-        <CardLine/>
-        {/* <div class="parallax"></div> */}
-        {this.Container()}
+        <CarouselHero />
+        <CardLine />
+        <div class="parallax"></div>
       </div>
     );
   }
