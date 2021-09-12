5adc3bf01a75b87930b7ea164171ad0bcfac8e74
Alex <revolalex@gmail.com>
2021-09-12 12:07:35 +0200

    test photo parralax

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Footer.css'
 import { withTranslation } from 'react-i18next';
-import watch from"../../img/watch.png"
+// import watch from"../../img/watch.png"
 import ExternaLinks from '../../utils/socialMedia.js'
 
 class Footer extends Component {
