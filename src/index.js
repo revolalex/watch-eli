5f5f4a4842aa6e469397131fe55e236fa69acfe4
Alex <revolalex@gmail.com>
2021-11-06 00:02:20 +0100

    push with build folder

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './i18n';
+import { BrowserRouter } from "react-router-dom";
 
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
       <Suspense fallback={null}>
         <App />
       </Suspense>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
