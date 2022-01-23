6c6365b15bc1a1386313992aa1a06e6a67b49f91
Alex <revolalex@gmail.com>
2022-01-23 16:42:45 +0100

    eli project with backup janury 2022

--- src/components/Buy/Chrono24.js
+++ src/components/Buy/Chrono24.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import chrono24 from '../../img/chrono24.png'
 import '../css/Chrono24.css'
 import { Container } from "react-bootstrap";
+import ExternaLinks from "../../utils/socialMedia";
 
 class Chrono24 extends Component {
     state = {}
@@ -9,7 +10,7 @@ class Chrono24 extends Component {
         return (
             <Container style={{textAlign:"center"}}>
                 <div  id="chrono24-div">
-                    <a href="https://www.chrono24.fr/" target="blank">
+                    <a href={ExternaLinks.chrono24} target="blank">
                         <img id="chrono24-img" src={chrono24} alt="chrono24" />
                     </a>
                 </div>
--- src/utils/socialMedia.js
+++ src/utils/socialMedia.js
@@ -2,8 +2,8 @@ class ExternaLinks{
     static facebook = 'https://m.facebook.com/WatchMarketClub/'
     static linkedin = "https://www.linkedin.com/company/watch-market-club"
     static google = "https://www.google.com/"
-    static chrono24 = "https://www.chrono24.fr/"
-    static instagram = "hhttps://www.instagram.com/watchmarketclub/?utm_medium=copy_link"
+    static chrono24 = "https://www.chrono24.fr/search/index.htm?customerId=28802&dosearch=true"
+    static instagram = "https://www.instagram.com/watchmarketclub/?utm_medium=copy_link"
     static youtube = "https://www.youtube.com/"
     static telephone = "+33 1 87 21 13 81"
 }
